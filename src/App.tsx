700b8fab45982ab2df3557256fe35810a9cf3a30
springdale95 <135840558+springdale95@users.noreply.github.com>
2025-02-08 10:11:48 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import styles from './App.module.scss';
+ import styles from './App.module.scss';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from './store/store';
